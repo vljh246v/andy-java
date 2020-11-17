7543f9e082397a8371d80b1cb116110ae0068496
demo.lim <demo.lim@kakaocorp.com>
2020-11-17 21:40:51 +0900

    project setting
    최초 프로젝트를 셋팅!

---
+++ src/test/java/com/bakeryblueprint/modernjava/ModernJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakeryblueprint.modernjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
