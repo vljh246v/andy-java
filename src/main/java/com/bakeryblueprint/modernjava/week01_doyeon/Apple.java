762d75d9bbbeb201335099225de37da1765ec9d1
doyeon <ryoody0313@gmail.com>
2020-12-14 20:57:31 +0900

    week01_doyeon homework

---
+++ src/main/java/com/bakeryblueprint/modernjava/week01_doyeon/Apple.java
@@ -0,0 +1,43 @@
+package com.bakeryblueprint.modernjava.week01_doyeon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Apple {
+    private String owner;
+    private String color;
+    private int weight;
+
+    public Apple() {}
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(owner).append(" : ").append(color).append(" : ").append(weight);
+        return builder.toString();
+    }
+}
