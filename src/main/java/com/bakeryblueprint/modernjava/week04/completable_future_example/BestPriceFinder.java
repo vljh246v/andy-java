dde3d146f49994d3e97e7db72e683e12450a4198
jaehyun <vljh246v@naver.com>
2020-12-23 03:19:02 +0900

    1. Shop  숫자 내 컴퓨터에 맞게 설정 2. CompletableFuture 사용한 병렬 처리 로직 작성

--- src/main/java/com/bakeryblueprint/modernjava/week04/completable_future_example/BestPriceFinder.java
+++ src/main/java/com/bakeryblueprint/modernjava/week04/completable_future_example/BestPriceFinder.java
@@ -2,20 +2,41 @@ package com.bakeryblueprint.modernjava.week04.completable_future_example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class BestPriceFinder {
 
-  private final List<Shop> shops = Arrays.asList(new Shop("BestPrice"),
-      new Shop("LetsSaveBig"),
-      new Shop("MyFavoriteShop"),
-      new Shop("BuyItAll"),
-      new Shop("ShopEasy"));
+  private final List<Shop> shops = Arrays.asList(
+      new Shop("shop1"),
+      new Shop("shop2"),
+      new Shop("shop3"),
+      new Shop("shop4"),
+      new Shop("shop5"),
+      new Shop("shop6"),
+      new Shop("shop7"),
+      new Shop("shop8"),
+      new Shop("shop9"),
+      new Shop("shop10"),
+      new Shop("shop11"),
+      new Shop("shop12"),
+      new Shop("shop13"));
 
   public List<String> findPrices(final String product) {
     return this.shops.parallelStream()
-        .map(it -> {
-          return it.getShopName() + " price is " + it.getPrice(product);
-        }).collect(Collectors.toList());
+        .map(it -> it.getShopName() + " price is " + it.getPrice(product))
+        .collect(Collectors.toList());
+  }
+
+  public List<String> findPricesFuture(final String product) {
+    final List<CompletableFuture<String>> priceFutures =
+        this.shops.stream()
+            .map(shop -> CompletableFuture.supplyAsync(() -> shop.getShopName() + " price is "
+                + shop.getPrice(product)))
+            .collect(Collectors.toList());
+
+    return priceFutures.stream()
+        .map(CompletableFuture::join)
+        .collect(Collectors.toList());
   }
 }
--- src/main/java/com/bakeryblueprint/modernjava/week04/completable_future_example/BestPriceFinderMain.java
+++ src/main/java/com/bakeryblueprint/modernjava/week04/completable_future_example/BestPriceFinderMain.java
@@ -5,9 +5,15 @@ public class BestPriceFinderMain {
   private static final BestPriceFinder bestPriceFinder = new BestPriceFinder();
 
   public static void main(final String[] args) {
-    final long start = System.nanoTime();
+    long start = System.nanoTime();
     BestPriceFinderMain.bestPriceFinder.findPrices("myPhone");
-    final long duration = (System.nanoTime() - start) / 1_000_000;
+    long duration = (System.nanoTime() - start) / 1_000_000;
+    System.out.println("Done in " + duration + "msecs");
+
+    // 왜 바꾸는 거지? -> CompletableFuture 형태를 사용하면 내가 정한 Executor 을 지정해 스레드 수를 정해줄 수 있음
+    start = System.nanoTime();
+    BestPriceFinderMain.bestPriceFinder.findPricesFuture("myPhone");
+    duration = (System.nanoTime() - start) / 1_000_000;
     System.out.println("Done in " + duration + "msecs");
   }
 
