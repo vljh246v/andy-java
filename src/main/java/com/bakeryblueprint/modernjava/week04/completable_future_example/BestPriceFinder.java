9a0da493912b8c83c07bccd9d97e44015d6f9813
jaehyun <vljh246v@naver.com>
2020-12-23 03:21:51 +0900

    shop 사이즈에 맞게 스레드를 활용하는 Executor 생성

--- src/main/java/com/bakeryblueprint/modernjava/week04/completable_future_example/BestPriceFinder.java
+++ src/main/java/com/bakeryblueprint/modernjava/week04/completable_future_example/BestPriceFinder.java
@@ -3,9 +3,12 @@ package com.bakeryblueprint.modernjava.week04.completable_future_example;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class BestPriceFinder {
+
 
   private final List<Shop> shops = Arrays.asList(
       new Shop("shop1"),
@@ -22,6 +25,16 @@ public class BestPriceFinder {
       new Shop("shop12"),
       new Shop("shop13"));
 
+
+  // shop 사이즈에 맞게 스레드를 활용하는 Executor 생성
+  private final Executor executor = Executors
+      .newFixedThreadPool(this.shops.size(), r -> {
+        final Thread t = new Thread(r);
+        // 데몬 스레드로 작성
+        t.setDaemon(true);
+        return t;
+      });
+
   public List<String> findPrices(final String product) {
     return this.shops.parallelStream()
         .map(it -> it.getShopName() + " price is " + it.getPrice(product))
@@ -32,7 +45,7 @@ public class BestPriceFinder {
     final List<CompletableFuture<String>> priceFutures =
         this.shops.stream()
             .map(shop -> CompletableFuture.supplyAsync(() -> shop.getShopName() + " price is "
-                + shop.getPrice(product)))
+                + shop.getPrice(product), this.executor))
             .collect(Collectors.toList());
 
     return priceFutures.stream()
