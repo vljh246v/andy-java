6e6ff24b5805f176b9f5e0f0c6533a6e2a3b7576
demo.lim <demo.lim@kakaocorp.com>
2021-01-25 21:47:59 +0900

    제네릭 클래스, 메서드 설계 정리

--- src/main/java/com/bakeryblueprint/modernjava/week08/jaehyun/GenericExample1.java
+++ src/main/java/com/bakeryblueprint/modernjava/week08/jaehyun/GenericExample1.java
@@ -1,2 +1,22 @@
-package com.bakeryblueprint.modernjava.week08.jaehyun;public class GenericExample1 {
+package com.bakeryblueprint.modernjava.week08.jaehyun;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GenericExample1 {
+
+  public static void main(String[] args) {
+    List myList = new ArrayList();
+    List<String> stringList = new ArrayList();
+    List<Integer> integerList = new ArrayList();
+
+    System.out.println(myList instanceof ArrayList);
+//    System.out.println(stringList instanceof ArrayList<String>);
+//    System.out.println(integerList instanceof ArrayList<Integer>);
+
+    Map<String, String> prop = new HashMap<>();
+    Map<String, String> sortedProp = Utils.sorting(prop);
+  }
 }
--- src/main/java/com/bakeryblueprint/modernjava/week08/jaehyun/Utils.java
+++ src/main/java/com/bakeryblueprint/modernjava/week08/jaehyun/Utils.java
@@ -1,2 +1,10 @@
-package com.bakeryblueprint.modernjava.week08.jaehyun;public class Utils {
+package com.bakeryblueprint.modernjava.week08.jaehyun;
+
+import java.util.Map;
+
+public class Utils {
+
+  public static <K, V> Map<K, V> sorting(Map<K, V> map){
+    return map;
+  }
 }
